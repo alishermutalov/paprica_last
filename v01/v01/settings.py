807b288c74bd9b7f35f9f458b2ddf541b9838ec9
DoniyorShuxratov <shuxratovdoni@gmail.com>
2024-01-06 01:13:58 +0500

    update

--- v01/v01/settings.py
+++ v01/v01/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY =config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['st40.online','www.st40.online', 'localhost']
 
