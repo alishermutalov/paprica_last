1700b45a347d71e8c9aa7f5ea240942f049b662f
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-01-06 13:38:56 +0500

    Added to admin pannel Service and Case

--- v01/posts/admin.py
+++ v01/posts/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import News, Contact
+from .models import News, Contact, Case , Service
 
 # Register your models here.
 
 admin.site.register(News)
 admin.site.register(Contact)
+admin.site.register(Case)
+admin.site.register(Service)
