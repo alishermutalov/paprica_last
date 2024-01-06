ee3d15fb937134d98baa99f4392489eaeb8077bf
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-01-06 13:37:34 +0500

    services/ cases/ urls added

--- v01/posts/urls.py
+++ v01/posts/urls.py
@@ -7,6 +7,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cases/', views.case_list, name='case_list'),
+    path('cases/<int:pk>/', views.case_detail, name='case_detail'),  # pk is the primary key of the case article
+    path('services/', views.service_list, name='service_list'),
+    path('services/<int:pk>/', views.service_detail, name='service_detail'),  # pk is the primary key of the service article
     path('news/', views.news_list, name='news_list'),
     path('news/<int:pk>/', views.news_detail, name='news_detail'),  # pk is the primary key of the news article
     path('contacts/', views.contacts_view, name='contacts'),
