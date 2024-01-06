3e72d0115223afde9078c9e7197a6a63f4d99c4f
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-01-06 13:39:20 +0500

    Added Case , Service models

--- v01/posts/models.py
+++ v01/posts/models.py
@@ -5,6 +5,35 @@ from ckeditor.fields import RichTextField
 
 # Create your models here.
 
+class Case(models.Model):
+    title = models.CharField(max_length = 255)  # The title of the news article
+    pub_date = models.DateTimeField(auto_now_add = True, verbose_name= "Published date")  # The publication date
+    content = RichTextField(config_name='awesome_ckeditor') # The full text content of the news article
+    source = models.URLField(max_length=200, blank=True, null = True)  # URL to the original source
+    image = models.ImageField(upload_to='images/', blank=True, null=True)  # An image for the article
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-pub_date']  # Orders the articles from most to least recent
+
+
+
+class Service(models.Model):
+    title = models.CharField(max_length = 255)  # The title of the news article
+    pub_date = models.DateTimeField(auto_now_add = True, verbose_name= "Published date")  # The publication date
+    content = RichTextField(config_name='awesome_ckeditor') # The full text content of the news article
+    source = models.URLField(max_length=200, blank=True, null = True)  # URL to the original source
+    image = models.ImageField(upload_to='images/', blank=True, null=True)  # An image for the article
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-pub_date']  # Orders the articles from most to least recent
+
+
 class News(models.Model):
     title = models.CharField(max_length = 255)  # The title of the news article
     author = models.CharField(max_length=100,blank=True, null = True)  # The author of the news article
